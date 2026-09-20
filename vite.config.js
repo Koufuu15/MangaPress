8711adfb0a5818d2ed4bf098b0cfa8f22dd3222b
Koufu <nadeshiko20091229@gmail.com>
2026-09-20 18:12:58 +0900

    デプロイ設定を変更

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/MangaDown/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/MangaPress/' : './',
   plugins: [
     vue(),
     vueDevTools(),
